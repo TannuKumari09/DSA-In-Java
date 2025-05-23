9927e48949e953066886b244708c0163a3955e5b
Tannu Kumari <tannu3132003@gmail.com>
2025-05-23 13:18:21 +0530

    Commit

---
+++ src/Recursion/Fibonacci.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    static int fib(int n){
+        if(n == 1 || n == 0){
+            return n;
+        }
+
+        int prev = fib(n-1);
+        int prevPrev = fib(n-2);
+        return prev + prevPrev;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Print value of n: ");
+        int n = sc.nextInt();
+        System.out.println("Fibonacci Series: ");
+        System.out.println(fib(n));
+    }
+}
