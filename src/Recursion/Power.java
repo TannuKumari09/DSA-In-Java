eb62141d9f9a71207f6a7177cd52224a010e5cb7
Tannu Kumari <tannu3132003@gmail.com>
2025-05-24 12:03:13 +0530

    Commit

--- src/Recursion/Power.java
+++ src/Recursion/Power.java
@@ -1,9 +1,21 @@
 package Recursion;
 
 public class Power {
+//    Method 1
+//    static int pow(int p, int q){
+//        if(q == 0) return 1;
+//        return pow(p, q-1) * p;
+//    }
+
+//    Method 2
     static int pow(int p, int q){
         if(q == 0) return 1;
-        return pow(p, q-1) * p;
+        if(q % 2 == 0){
+            return pow(p, q/2) * pow(p, q/2);
+        }
+        else{
+            return p * pow(p, q/2) * pow(p, q/2);
+        }
     }
     public static void main(String[] args) {
         System.out.println(pow(5,4));
